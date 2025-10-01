dba07c2eb3e7ddae634db044a0aa0d3b458486e2
SudhanshuBaberwal <24bcs147@iiitdwd.ac.in>
2025-10-02 03:13:48 +0530

    Initialize Frontend

---
+++ frontend/AdvanceAuth/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+    <div className='text-8xl'>
+      <h1 className='text-red-500'>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
